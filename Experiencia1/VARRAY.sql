054c938c9c279df6148a4f86bd573afc47c60737
vice. <vice.alvarezm@duocuc.cl>
2025-08-14 15:45:05 -0400

    Update VARRAY.sql

--- Experiencia1/VARRAY.sql
+++ Experiencia1/VARRAY.sql
@@ -21,20 +21,23 @@ DECLARE
         empleado e
     INNER JOIN comuna c ON e.id_comuna = c.id_comuna
     INNER JOIN tipo_salud tp ON tp.cod_tipo_sal = e.cod_tipo_sal
-    INNER JOIN afp a ON e.cod_afp = a.cod_afp;
+    INNER JOIN afp a ON e.cod_afp = a.cod_afp
+    order by 1;
 BEGIN
     varray_a := varray_bono(50000,100000);
     
+    EXECUTE IMMEDIATE('truncate table liquidacion_sueldo');
+    
     FOR i in c_empleado LOOP
     
-    IF i.sueldo_base < varray_a(2) THEN
-        i.sueldo_base:= i.sueldo_base+varray_a(1);
+    IF i.sueldo_base < 1000000 THEN
+        i.sueldo_total:= i.sueldo_total+varray_a(1);
     ELSE
-        i.sueldo_base:= i.sueldo_base+varray_a(2);
+        i.sueldo_total:= i.sueldo_total+varray_a(2);
     END IF;
     
     i.sueldo_total:=i.sueldo_base+i.bono_annios_trabajo-i.afp-i.salud;
-
+    
         dbms_output.put_line('');
         dbms_output.put_line('Run : '||i.run);
         dbms_output.put_line('Nombre Completo : '||i.nombre_completo);
@@ -47,18 +50,7 @@ BEGIN
         dbms_output.put_line('AFP : '||i.afp);
         dbms_output.put_line('Sueldo Total : '||i.sueldo_total);
         
-    INSERT INTO liquidacion_sueldo 
-     VALUES ( i.id_emp,
-           i.run,
-           i.nombre_completo,
-           i.direccion,
-           i.comuna,
-           i.sueldo_base,
-           i.anios_trabajo,
-           i.bono_annios_trabajo,
-           i.salud,
-           i.afp,
-           i.sueldo_total);
+    
     END LOOP;
 
 END;
